85f0244870d8429e91df4f1f417a5b75ae1aa45d
sainsh <kfsainsh@gmail.com>
2020-04-24 12:32:56 +0200

    added .gitignore with appsettings.json so database connection string isn't changed all the time
    don't know if this will mock it up

--- Migrations/20200424102655_initialMigration.Designer.cs
+++ Migrations/20200424102655_initialMigration.Designer.cs
@@ -4,14 +4,16 @@ using MVC_projekt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MVC_projekt.Migrations
 {
     [DbContext(typeof(MVCMovieContext))]
-    partial class MVCMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200424102655_initialMigration")]
+    partial class initialMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
