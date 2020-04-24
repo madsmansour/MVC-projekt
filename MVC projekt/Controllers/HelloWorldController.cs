b635860889f26fbb26f6e4b37c930a79611dab65
Mads Mansour Larsen <42993941+madsmansour@users.noreply.github.com>
2020-04-24 12:16:15 +0200

    Add project files.

---
+++ MVC projekt/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MVC_projekt.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /helloworld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: /helloworld/welcome/
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ MVC projekt/Models/SeedData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MVC_projekt.Data;
+
+namespace MVC_projekt.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MVCMovieContext(serviceProvider.GetRequiredService<DbContextOptions<MVCMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(new Movie { Title = "Johhny Bravo", ReleaseDate = DateTime.Parse("1995-05-05"), Genre = "Cartoon", Price = 7.99M }, 
+                    new Movie { Title = "Johhny Alpha", ReleaseDate = DateTime.Parse("1991-05-05"), Genre = "Cartoon", Price = 9.99M },
+                    new Movie { Title = "Johhny Charlie", ReleaseDate = DateTime.Parse("1992-05-05"), Genre = "Cartoon", Price = 10.99M });
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
